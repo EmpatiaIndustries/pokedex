b82832aa13714b4e5962464f199d130a2d46f133
Robert <robert.sepulveda1995@gmail.com>
2020-09-11 00:06:29 -0300

    se hace commit prueba

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Container from "../components/container";
 import Head from "next/head";
 import fetch from "isomorphic-fetch";
 import Posts from "../components/Posts";
+
 const Index = (props) => {
   return (
     <Container>
@@ -18,11 +19,6 @@ const Index = (props) => {
 Index.getInitialProps = async (ctx) => {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const data = await res.json();
-  let results = data.results;
-
-  results.forEach((poke) => {
-    let url = poke.url;
-  });
 
   return { pokemon: data.results };
 };
